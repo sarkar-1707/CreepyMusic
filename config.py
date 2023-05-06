e2947e5bbffba6bed1d3b0c04dd533d75128368f
sarkar-1707 <132136826+sarkar-1707@users.noreply.github.com>
2023-05-06 15:07:41 +0530

    Update config.py
    Signed-off-by: sarkar-1707 <132136826+sarkar-1707@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/e1720309d6dc21da3cfcb.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/e1720309d6dc21da3cfcb.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/creepy_world_hell")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/old_buddies")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5207435291").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
